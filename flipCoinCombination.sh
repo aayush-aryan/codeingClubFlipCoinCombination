9157d48a9f97c8926a7b8152403beb7b8390497d
unknown <log2aayusharyan@gmail.com>
2020-09-06 04:18:50 -0700

    determining the percentage of singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,10 +2,29 @@
 
 echo "Welcome to Flip Coin Combination"
 HEAD=0
-flipCoin=$((RANDOM%2))
-if [ $flipCoin == $HEAD ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+
+headCount=0
+tailCount=0
+
+declare -A singletFlip
+
+read -p "Enter the Number of Coin Flip : " numberOfCoinFlip
+
+#for storing headCount and tailCount in dictionary
+for(( count=0; count<$numberOfCoinFlip; count++ ))
+do
+   FlipCoin=$(( RANDOM % 2 ))
+
+   if [ $FlipCoin -eq $HEAD ]
+   then
+      singletFlip[HEAD]=$((++headCount))
+   else
+      singletFlip[TAIL]=$((++tailCount))
+   fi
+done
+
+singletHeadPercentage=`echo "scale=2; $headCount * 100 / $numberOfCoinFlip" | bc`
+singletTailPercentage=`echo "scale=2; $tailCount *100 / $numberOfCoinFlip" | bc`
+
+echo "To single head percentage and head count:$headCount : " $singletHeadPercentage
+echo "To single tail percentage and tail count:$tailCount : " $singletTailPercentage
